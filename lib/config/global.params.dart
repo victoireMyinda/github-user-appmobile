2f4cb67af2bba233a1fe9973811f9b2c5eabca62
victoireMyinda <myindavictoire@gmail.com>
2024-08-09 00:06:28 +0200

    main:update

--- lib/config/global.params.dart
+++ lib/config/global.params.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class GlobalParams {
   static List<Map<String, dynamic>> menus = [
-    {"title": "Counter", "icon": const Icon(Icons.home), "route": "/counter"},
-    {"title": "Meteo", "icon": const Icon(Icons.camera), "route": "/counter"},
-    {"title": "Gallery", "icon": const Icon(Icons.map), "route": "/counter"},
+    {"title": "Home", "icon": Icons.home, "route": "/home"},
+    {"title": "Meteo", "icon": Icons.camera, "route": "/meteo"},
+    {"title": "Gallery", "icon": Icons.map, "route": "/gallery"},
   ];
 }
--- lib/presentation/widgets/drawer.widget.dart
+++ lib/presentation/widgets/drawer.widget.dart
@@ -1,49 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:formation/config/global.params.dart';
 
 class myDrawer extends StatelessWidget {
   const myDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-     return Drawer(
-    child: ListView(
-      children: const [
-        DrawerHeader(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepOrange,
-                Colors.deepPurple
-              ],
-            )
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Colors.deepOrange, Colors.deepPurple],
+            )),
+            child: CircleAvatar(
+              child: Icon(Icons.person),
+            ),
           ),
-          child: CircleAvatar(
-            child: Icon(Icons.person),
-          ),
-        ),
-        ListTile(
-          title: Text("Counter"),
+         
           
-          trailing: Icon(Icons.quiz, color: Colors.deepPurpleAccent,),
-        ),
-        Divider(
-          height: 2,
-        ),
-        ListTile(
-          title: Text("Galery"),
+          ...(GlobalParams.menus as List).map((item) {
+            return  ListTile(
+              title: Text(item["title"]),
+             trailing: Icon(
+                item["icon"],
+                color: Colors.deepPurpleAccent,
+              ),
+              onTap :() {
+                 Navigator.of(context).pushNamed(item["route"] as String);
+              },
+            );
           
-          trailing: Icon(Icons.image,color: Colors.deepPurpleAccent,),
-        ),
-        Divider(
-          height: 2,
-        ),
-        ListTile(
-          title: Text("Meteo"),
-          
-          trailing: Icon(Icons.sunny,color: Colors.deepPurpleAccent,),
-        ),
-      ],
-    ),
-  );
+          }),
+         const  Divider(height: 2,)
+        ],
+      ),
+    );
   }
 }
